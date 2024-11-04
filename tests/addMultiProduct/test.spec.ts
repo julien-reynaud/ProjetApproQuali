31f0bb155fbdeba391ba2a5b40084c48cbcc5d40
Julien <julien.reynaud@student.junia.com>
2024-11-04 14:57:26 +0100

    allure fini

--- tests/addMultiProduct/test.spec.ts
+++ tests/addMultiProduct/test.spec.ts
@@ -3,7 +3,7 @@ import {expect} from "@playwright/test";
 import {addMultiProduct} from "./addMultiProduct";
 
 test.describe('Prix panier', () => {
-    test('vérification du prix du panier', async ({ page, verifyCartProductQuantity }) => {
+    test('vérification du prix du panier', {tag : ['@AMP-dev', '@AMP-sit']}, async ({ page, verifyCartProductQuantity }) => {
         // Naviguer vers la page cible
         await page.goto('https://amazon.fr');
 
